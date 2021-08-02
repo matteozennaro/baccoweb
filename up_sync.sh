39820a59453e604ce467793c932d4fd808875ce2
Matteo Zennaro <matteo.zennaro90@gmail.com>
2021-08-02 10:09:26 +0200

    update up sync

--- up_sync.sh
+++ up_sync.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rsync -avt --exclude '*gpy*' --exclude '*SFR*' ./ bacco@82.223.84.172:/var/www/vhosts/bacco.dipc.org/httpdocs/
+rsync -avt --exclude '*gpy*' --exclude '*SFR*' --exclude '.git/' ./ bacco@82.223.84.172:/var/www/vhosts/bacco.dipc.org/httpdocs/
